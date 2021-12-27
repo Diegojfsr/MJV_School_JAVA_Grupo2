bbd80e6bee713cf2edbea8abd3128c6b3337f1aa
Diego Jefferson <diegojfsr@gmail.com>
2021-12-26 22:15:21 -0300

    Exemplo-classe-Wrappers

---
+++ Wrappers.java
@@ -0,0 +1,43 @@
+// Wrapper Classes Envólucros de Tipos Primitivos
+
+// Classes Wrappers
+
+/*  Empacotadores do tipo primitivo:
+Integer - Long - Character - Byte - Float - Void - Short - Double - Boolean
+
+Essas classes suprem a necessidade  e oferecem alguns recursos, usados na conversão de tipos de valores em outros tipos.
+Exe. Uma estring em double.
+
+Tipos Primitivos: Variaveis
+{
+int idade = 31;
+double preço = 12.45
+char sexo = 'M'
+boolean casado = true; 
+}
+
+Tipos com a Classe Wrapper
+{
+   Integer idade = new Integer(31);
+   Double preço = new Double(12.45);
+   Char sexo = new Char(M)
+
+}
+
+Outras Posibilidade
+{
+    Double preço = new Double(12.45); // valor Double
+    Double preço = new Double("12.45"); // valor em String / converte o valor double em valor string
+    double d = preço.doubleValue(); // recupera o valor double de preço
+    int i = preço.intValue(); // valor em inteiro / converte o valor double em valor inteiro
+    byte b = preço.byteValue(); // valor byte / converte o valor double em valor inteiro
+}
+
+{
+
+}
+
+
+
+
+*/
