89540b108fca3e5d9df84583e893245342b33d5c
Diego Jefferson <diegojfsr@gmail.com>
2021-12-28 15:44:37 -0300

    Calculadora

---
+++ Calculadora/CalculadoraTest.java
@@ -0,0 +1,8 @@
+public class CalculadoraTest {
+    public static void main(String[] args) {
+        Calculadora cal = new Calculadora();
+
+        calc.soma(1,2);
+        calc.soma(1.0, 2.0);
+    }
+}
